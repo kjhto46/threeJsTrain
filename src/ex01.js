4c4535ab299e3e6baa9189ddabee16f528665e6b
kjhto46 <tony1996kjh@gmail.com>
2023-10-15 16:56:01 +0900

    환경 변환 축, 그리드 헬퍼를 이용하여 도움 받기, camera.lookAt으로 방향보게 하기

--- src/ex01.js
+++ src/ex01.js
@@ -1,59 +1,85 @@
-import * as THREE from "three";
+import * as THREE from 'three';
+
+// ----- 주제: AxesHelper 축 헬퍼, GridHelper 격자무늬 헬퍼
+/** AXIS는 축을 이야기 하는 것 (AXES는 복수형)
+ * Grid는 격자무늬 그리드 가이드를 이야기 하는것
+ */
 
 export default function example() {
-  // 동적으로 캔버스 조립하기
-  // const renderer = new THREE.WebGLRenderer();
-  // renderer.setSize(window.innerWidth, window.innerHeight);
-  // document.body.appendChild(renderer.domElement)
+	// Renderer
+	const canvas = document.querySelector('#three-canvas');
+	const renderer = new THREE.WebGLRenderer({
+		canvas,
+		antialias: true
+	});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-  const canvas = document.querySelector("#three-canvas");
-  const renderer = new THREE.WebGLRenderer({
-    canvas,
-    antialias: true,
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+	// Scene
+	const scene = new THREE.Scene();
 
-  // Scene
-  const scene = new THREE.Scene();
+	// Camera
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	camera.position.x = 1;
+	camera.position.y = 3;
+	camera.position.z = 0;
+	scene.add(camera);
 
-  // Camera
-  // PerspectiveCamera(원근 카메라)
-  const camera = new THREE.PerspectiveCamera(
-    75, // 시야각 (field of wiew)
-    window.innerWidth / window.innerHeight, //종횡비 (aspect)
-    0.1, // near
-    1000 // far
-  );
-  camera.position.x = 1;
-  camera.position.y = 2;
-  camera.position.z = 5;
-  scene.add(camera);
+	// Light
+	const ambientLight = new THREE.AmbientLight('white', 0.5);
+	scene.add(ambientLight);
 
-  // OrthographicCamera (직교카메라)
-  // const camera = new THREE.OrthographicCamera(
-  //    -(window.innerWidth / window.innerHeight), //Left
-  //    window.innerWidth / window.innerHeight, //Right
-  //    1, //top
-  //    -1, //Bottom
-  //    0.1,
-  //    1000
-  // )
-  // camera.position.x = 1;
-  // camera.position.y = 2;
-  // camera.position.z = 5;
-  // camera.lookAt(0,0,0);
-  // camera.zoom = 0.5; //z 축 이동으로는 줌인,줌아웃이 아님. z축 시점이 변경 zoom으로 제어
-  // camera.updateProjectionMatrix(); //zoom 하고 난뒤 update해야함.
-  // scene.add(camera);
+	const directionalLight = new THREE.DirectionalLight('white', 1);
+	directionalLight.position.x = 1;
+	directionalLight.position.z = 2;
+	scene.add(directionalLight);
 
-  // Mesh
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({
-    color: "#ff0000",
-  });
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+	//!! AxesHelper
+	const axesHelper = new THREE.AxesHelper(3);
+	scene.add(axesHelper);
 
-  // 그리기
-  renderer.render(scene, camera);
+	//!! GridHelper
+	const gridHelper = new THREE.GridHelper(5);
+	scene.add(gridHelper);
+
+	// Mesh
+	const geometry = new THREE.BoxGeometry(1, 1, 1);
+	const material = new THREE.MeshStandardMaterial({
+		color: 'seagreen'
+	});
+	const mesh = new THREE.Mesh(geometry, material);
+	mesh.position.x = 2;
+	mesh.position.z = 2;
+	scene.add(mesh);
+
+	// !! mesh.position이 지정된 이후에 작성해야함
+	camera.lookAt(mesh.position);
+	// 그리기
+	const clock = new THREE.Clock();
+
+	function draw() {
+		const time = clock.getElapsedTime();
+
+		mesh.rotation.y = time;
+
+		renderer.render(scene, camera);
+		renderer.setAnimationLoop(draw);
+	}
+
+	function setSize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.render(scene, camera);
+	}
+
+	// 이벤트
+	window.addEventListener('resize', setSize);
+
+	draw();
 }
