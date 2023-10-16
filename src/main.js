5151b2fa475dff6c2c77f4a69e8689d1d4831614
kjhto46 <tony1996kjh@gmail.com>
2023-10-16 18:12:18 +0900

    크기조정 회전 그룹 만들기

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 // import example from './ex03';
 
 // transform(변환)
-import example from './ex04'; 
+// import example from './ex04'; // position
+// import example from './ex05'; // scale
+// import example from './ex06'; // rotation 회전
+import example from './ex07'; //그룹만들기 
+
 
 example();
